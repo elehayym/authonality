30cc2045e0bc7b9f48451ae2c2429a80ba3bfb1e
think <think@www6319uj.sakura.ne.jp>
2013-04-30 12:37:39 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Authonality::Application.config.secret_token = '8708d6813bb3e6221d1e05c33d70a5ce177f263a0a9ac79acb978182d7ae89929e74511c4c29ec32d5f4426c64ef8ed6b6815c57697bc01b4b63b617a3139838'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Authonality::Application.config.session_store :cookie_store, key: '_authonality_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Authonality::Application.config.session_store :active_record_store
